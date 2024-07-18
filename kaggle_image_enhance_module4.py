87da3fe471039f2f82401a8c1666832e1364dbe1
Karan Rajgor <165757743+KaranRajgorTreta@users.noreply.github.com>
2024-07-18 17:20:07 +0530

    achived proper blured out image

--- kaggle_image_enhance_module4.py
+++ kaggle_image_enhance_module4.py
@@ -19,4 +19,46 @@ plt.subplot(131); plt.imshow(nz_img_darker); plt.title("Darker")
 plt.subplot(132); plt.imshow(nz_img_brighter); plt.title("Brighter")
 plt.subplot(133); plt.imshow(nz_img_rgb); plt.title("Original")
 
+matrix1 = np.ones(nz_img_rgb.shape) * .8
+matrix2 = np.ones(nz_img_rgb.shape) * 1.2
+
+img_rgb_darker   = np.uint8(cv2.multiply(np.float64(nz_img_rgb), matrix1))
+img_rgb_brighter = np.uint8(cv2.multiply(np.float64(nz_img_rgb), matrix2))
+
+# Show the images
+plt.figure(figsize=[18,5])
+plt.subplot(131); plt.imshow(img_rgb_darker);  plt.title("Lower Contrast")
+plt.subplot(132); plt.imshow(nz_img_rgb);         plt.title("Original")
+plt.subplot(133); plt.imshow(img_rgb_brighter);plt.title("Higher Contrast")
+
+
+matrix1 = np.ones(nz_img_rgb.shape) * .8
+matrix2 = np.ones(nz_img_rgb.shape) * 1.2
+
+img_rgb_lower   = np.uint8(cv2.multiply(np.float64(nz_img_rgb), matrix1))
+img_rgb_higher  = np.uint8(np.clip(cv2.multiply(np.float64(nz_img_rgb), matrix2),0,255))
+
+# Show the images
+plt.figure(figsize=[18,5])
+plt.subplot(131); plt.imshow(img_rgb_lower);  plt.title("Lower Contrast")
+plt.subplot(132); plt.imshow(nz_img_rgb);     plt.title("Original")
+plt.subplot(133); plt.imshow(img_rgb_higher); plt.title("Higher Contrast")
+
+
+bw_img = cv2.imread('images/image_blur_6.jpg', cv2.IMREAD_GRAYSCALE)
+retval, bw_img_thresh = cv2.threshold(bw_img, thresh=100, maxval=255, type=cv2.THRESH_BINARY)
+
+# Show the images
+plt.figure(figsize=[18,5])
+plt.subplot(121); plt.imshow(bw_img, cmap="gray"); plt.title("Original")
+plt.subplot(122); plt.imshow(bw_img_thresh, cmap="gray"); plt.title("Thresholded")
+
+bw_img = cv2.imread('images/image_blur_6.jpg', cv2.IMREAD_GRAYSCALE)
+bw_img_thresh = cv2.adaptiveThreshold(bw_img, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=11, C=2)
+
+# Show the images
+plt.figure(figsize=[18,5])
+plt.subplot(121); plt.imshow(bw_img, cmap="gray"); plt.title("Original")
+plt.subplot(122); plt.imshow(bw_img_thresh, cmap="gray"); plt.title("Thresholded")
+
 plt.show()  # Display the plots
